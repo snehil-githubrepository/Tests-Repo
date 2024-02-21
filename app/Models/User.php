0cf2f99ff4b2b07328473d7dbd4c815c43c21ac5
Snehil <snehil.sharma020901@gmail.com>
2024-02-21 11:41:30 +0530

    Changed configuration of User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,17 +20,7 @@ class User extends Authenticatable
     ];
 
     protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
+        'password'
     ];
 
     public function products()
