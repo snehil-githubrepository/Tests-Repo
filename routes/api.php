04cb46ad9b3f432129932f3f8eccd86704f24208
Snehil <snehil.sharma020901@gmail.com>
2024-02-21 15:46:31 +0530

    Added the api route file for AuthController

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,22 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 //new api routes
+
+//Auth Routes
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout']);
+
+Route::get('/profile/{id}', [AuthController::class, 'showProfile']);
+Route::put('/profile/{id}', [AuthController::class, 'updateProfile']);
+Route::delete('/profile/{id}', [AuthController::class, 'deleteProfile']);
+
+//Management Routes
+// Route::get([ManagementController::class, '']);
+// Route::get([ManagementController::class, '']);
+// Route::get([ManagementController::class, '']);
+// Route::get([ManagementController::class, '']);
+// Route::get([ManagementController::class, '']);
