af2422b59e5ee628414254b597eb39c88575f120
Snehil <snehil.sharma020901@gmail.com>
2024-02-22 14:20:48 +0530

    New Routes for Management Controller Added

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ManagementController;
 
 //new api routes
 
@@ -16,8 +17,10 @@ Route::put('/profile/{id}', [AuthController::class, 'updateProfile']);
 Route::delete('/profile/{id}', [AuthController::class, 'deleteProfile']);
 
 //Management Routes
-// Route::get([ManagementController::class, '']);
-// Route::get([ManagementController::class, '']);
-// Route::get([ManagementController::class, '']);
-// Route::get([ManagementController::class, '']);
-// Route::get([ManagementController::class, '']);
+
+Route::post('/product/store', [ManagementController::class, 'storeProduct']);
+Route::put('/product/update/{id}', [ManagementController::class, 'updateProduct'])->middleware('admin');
+Route::get('/product/show/{id}', [ManagementController::class, 'showProduct']);
+Route::get('/products', [ManagementController::class, 'showAllProducts'])->middleware('admin');
+Route::delete('/product/{id}',[ManagementController::class, 'deleteProduct'])->middleware('admin');
+Route::get('/products/search', [ManagementController::class, 'searchProducts']);
